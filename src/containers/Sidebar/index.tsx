6ecec882df80b9ce31deb52f5910402285078ca6
hibernon rodrigues <hibernon73@gmail.com>
2024-09-16 12:58:15 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,24 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/hiberNT.png" />
-    <Titulo fontSize={20}>Hibernon Neto</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Hibernon Neto</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        hiberNT
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
 export default Sidebar
+//SidebarContainer foi feito para ser um div do sidebar para podermo estlizar para quem o sidebar acompanhe a pagina na rolagem do site se n tivesse essa "div" q é o SidebarContainer n teriamos como pois o aside acompanha a alltura do site
